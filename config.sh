601dd5be8b4291d512c93a2d6982465ec62c0f7f
Justin Merz <jrmerz@gmail.com>
2021-02-22 09:17:51 -0800

    cleanup

--- config.sh
+++ config.sh
@@ -4,15 +4,23 @@
 # Setup your application deployment here
 ################################
 
+# Grab build number is mounted in CI system
+if [[ -f /config/.buildenv ]]; then
+  source /config/.buildenv
+else
+  BUILD_NUM=-1
+fi
+
 # Main version number we are tagging the app with. Always update
 # this when you cut a new version of the app!
-APP_VERSION=v1.3.2-rc1
+APP_TAG=v2.0-alpha
+APP_VERSION=${APP_TAG}.${BUILD_NUM}
 
-CORE_SERVER_REPO_TAG=dev
-UCD_LIB_SERVER_REPO_TAG=dev
-LORIS_SERVICE_REPO_TAG=v1.0.0
-TESSERACT_SERVICE_REPO_TAG=v1.0.1
-CAS_SERVICE_REPO_TAG=v1.0.1
+CORE_SERVER_REPO_TAG=v2.0-dev
+UCD_LIB_SERVER_REPO_TAG=v2.0-dev
+LORIS_SERVICE_REPO_TAG=main
+TESSERACT_SERVICE_REPO_TAG=main
+CAS_SERVICE_REPO_TAG=main
 
 #### End main config ####
 
@@ -42,27 +50,40 @@ CAS_SERVICE_REPO_URL=$GITHUB_ORG_URL/$CAS_SERVICE_REPO_NAME
 
 # Docker Hub
 UCD_LIB_DOCKER_ORG=ucdlib
-DOCKER_CACHE_TAG="latest-build"
+
+DOCKER_CACHE_TAG="latest"
+LOCAL_TAG="local-dev"
 
 # Docker Images
-FCREPO_IMAGE_NAME=fin-fcrepo
-POSTGRES_IMAGE_NAME=fin-postgres
-NODE_UTILS_IMAGE_NAME=fin-node-utils
-SERVER_IMAGE_NAME=fin-server
-TRUSTED_PROXY_IMAGE_NAME=fin-trusted-proxy
-ELASTIC_SEARCH_IMAGE_NAME=fin-elasticsearch
-UCD_LIB_SERVER_IMAGE_NAME=fin-ucd-lib-server
-ESSYNC_IMAGE_NAME=fin-essync
-UCD_LIB_CLIENT_IMAGE_NAME=fin-ucd-lib-client
-LORIS_IMAGE_NAME=fin-loris
-TESSERACT_IMAGE_NAME=fin-tesseract
-CAS_IMAGE_NAME=fin-cas
-UCD_LIB_SERVER_IMPL=fin-ucd-lib-server-impl
+FCREPO_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-fcrepo
+POSTGRES_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-postgres
+NODE_UTILS_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-node-utils
+SERVER_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-server
+TRUSTED_PROXY_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-trusted-proxy
+ELASTIC_SEARCH_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-elasticsearch
+UCD_LIB_SERVER_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-ucd-lib-server
+ESSYNC_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-essync
+UCD_LIB_CLIENT_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-ucd-lib-client
+LORIS_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-loris
+TESSERACT_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-tesseract
+CAS_IMAGE_NAME=$UCD_LIB_DOCKER_ORG/fin-cas
+UCD_LIB_SERVER_IMPL=$UCD_LIB_DOCKER_ORG/fin-ucd-lib-server-impl
 
+ALL_DOCKER_BUILD_IMAGES=( \
+ $FCREPO_IMAGE_NAME $POSTGRES_IMAGE_NAME $NODE_UTILS_IMAGE_NAME \
+ $SERVER_IMAGE_NAME $TRUSTED_PROXY_IMAGE_NAME $ELASTIC_SEARCH_IMAGE_NAME \
+ $UCD_LIB_SERVER_IMAGE_NAME $ESSYNC_IMAGE_NAME $UCD_LIB_CLIENT_IMAGE_NAME \
+ $LORIS_IMAGE_NAME $TESSERACT_IMAGE_NAME $CAS_IMAGE_NAME $UCD_LIB_SERVER_IMPL \
+)
 
 # Git
 GIT=git
 GIT_CLONE="$GIT clone"
+
+ALL_GIT_REPOSITORIES=( \
+ $CORE_SERVER_REPO_NAME $UCD_LIB_SERVER_REPO_NAME $LORIS_SERVICE_REPO_NAME \
+ $TESSERACT_SERVICE_REPO_NAME $CAS_SERVICE_REPO_NAME \
+)
 
 # directory we are going to cache our various git repos at different tags
 # if using pull.sh or the directory we will look for repositories (can by symlinks)
